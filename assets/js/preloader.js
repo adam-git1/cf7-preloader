eb924d94a71055896adbcf6f273aea6783dcd098
ChesterX <50480774+ChesterX@users.noreply.github.com>
2025-12-01 13:14:20 +0200

    Fixes

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -3,23 +3,21 @@
     
     var preloaderHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
     
-    $(document).on('wpcf7submit', '.wpcf7-form', function() {
+    // Show preloader only after successful validation (before sending to server)
+    $(document).on('wpcf7beforesubmit', '.wpcf7-form', function() {
         var $form = $(this);
         var $responseOutput = $form.find('.wpcf7-response-output');
         var $wrapper = $form.find('.cf7-preloader-wrapper');
         
         if ($wrapper.length === 0) {
             $wrapper = $('<div class="cf7-preloader-wrapper"></div>');
-            if ($responseOutput.length) {
-                $responseOutput.after($wrapper);
-            } else {
-                $form.append($wrapper);
-            }
+            $form.append($wrapper);
         }
         
         $wrapper.html(preloaderHTML).addClass('active');
     });
     
+    // Hide preloader on any completion (success, error, spam, invalid form)
     $(document).on('wpcf7mailsent wpcf7mailfailed wpcf7spam wpcf7invalid', '.wpcf7-form', function() {
         var $form = $(this);
         var $wrapper = $form.find('.cf7-preloader-wrapper');
--- cf7-preloader.php
+++ cf7-preloader.php
@@ -3,7 +3,7 @@
  * Plugin Name: CF7 Preloader
  * Plugin URI: https://github.com/adam-git1/cf7-preloader
  * Description: Adds preloader when submitting Contact Form 7 forms
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: adam-git1
  * Author URI: https://github.com/adam-git1
  * License: GPL v2 or later
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 
 define('CF7_PRELOADER_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CF7_PRELOADER_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('CF7_PRELOADER_VERSION', '1.0.2');
+define('CF7_PRELOADER_VERSION', '1.0.3');
 
 class CF7_Preloader {
     
